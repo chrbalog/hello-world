eb7bbcf7bcbe948a8e2f570391234b9a83006acc
chrbalog <45148062+chrbalog@users.noreply.github.com>
2018-11-19 18:44:28 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<form action='index.php' method='get'>
+<h1>Langweilige Begrüßung:</h1>
+Name:
+    <input type=text name='name'><br>
+    
+
+Begrüßung: 
+<select name="greetings">
+  <option  value="g01">Guten Morgen</option>
+  <option  value="g02">Mahlzeit</option>
+  <option  value="g03">Guten Abend</option>
+  <option  value="g04">Gute Nacht</option>
+</select>
+<br><br><br>
+
+<h1>Krasse Begrüßung:</h1>
+<br>
+<select name="begrüßung">
+  <option  value="be01">Gudä Morgä</option>
+  <option  value="be02">Moahlzeit</option>
+  <option  value="be03">Oabend</option>
+  <option  value="be04">Hey</option>
+</select>
+
+<select name="anrede">
+  <option  value="an01">Alfi</option>
+  <option  value="an02">Ralfi</option>
+  <option  value="an03">Lisa</option>
+  <option  value="an04">Isa</option>
+  <option  value="an05">Alter</option>
+  <option  value="an06">Walter</option>
+</select>
+
+<select name="spruch">
+  <option  value="sp01">du harter Hund</option>
+  <option  value="sp02">du fleißige Biene</option>
+  <option  value="sp03">du toller Hecht</option>
+  <option  value="sp04">du blindes Huhn</option>
+</select>
+<br><br>
+   <input type=submit value='Lätz fätz'>
+
+</form>
+
+    
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+
+<?php 
+echo $_REQUEST['greetings'];
+echo $_REQUEST['name'];
+	
+
+?>
+
+
+</body>
+</html>
+
+
+ 
